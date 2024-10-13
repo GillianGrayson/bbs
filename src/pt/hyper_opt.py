5d0a3b3ce92d596337b1e5a77ebcedfcb6af0b10
Igor <hewitt.archie@yandex.ru>
2024-10-13 23:17:49 +0300

    Mriya first models

--- src/pt/hyper_opt.py
+++ src/pt/hyper_opt.py
@@ -70,7 +70,7 @@ def get_model_config_trial(
     elif model_config_default._model_name == 'DANetModel':
         model_config['n_layers'] = trial.suggest_int('n_layers', 16, 32)
         model_config['abstlay_dim_1'] = trial.suggest_categorical('abstlay_dim_1', [8, 16, 32])
-        model_config['k'] = trial.suggest_int('k', [2, 3, 4])
+        model_config['k'] = trial.suggest_int('k', 2, 4, step=1)
         model_config['dropout_rate'] = trial.suggest_float('dropout_rate', 0.0, 0.25)
         model_config['learning_rate'] = trial.suggest_float('learning_rate', 0.00001, 1.0, log=True)
     elif model_config_default._model_name == 'CategoryEmbeddingModel':
