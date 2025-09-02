f5488da3f3ef63d668cd5dc842d3a2d2ce9e20de
Igor Yusipov <hewitt.archie@yandex.ru>
2025-09-02 15:54:55 +0300

    ChAMP install script

---
+++ notebooks/r/champ.R
@@ -0,0 +1,19 @@
+rm(list=ls())
+
+
+if (!requireNamespace("BiocManager", quietly=TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("ChAMP")
+BiocManager::install("ChAMPdata")
+BiocManager::install("IlluminaHumanMethylation450kanno.ilmn12.hg19")
+BiocManager::install("IlluminaHumanMethylationEPICanno.ilm10b4.hg19")
+BiocManager::install("org.Hs.eg.db")
+BiocManager::install("IlluminaHumanMethylationEPICmanifest")
+BiocManager::install("IlluminaHumanMethylation450kmanifest")
+BiocManager::install("geneLenDataBase")
+BiocManager::install("GO.db")
+install.packages("devtools")
+devtools::install_version("kpmt",version="0.1.0")
+
+library(ChAMP)
